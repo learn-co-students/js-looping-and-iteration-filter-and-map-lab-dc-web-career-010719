2cb1a1711807e7620353e4929e7db09d7e30f09a
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-02-17 13:06:56 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function (driver) {return driver.revenue > revenue;});
+};
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  let newDrivers = driversWithRevenueOver(drivers, revenue);
+  return newDrivers.map(function (driver) {return driver.name;});
+};
+
+function exactMatch(drivers, obj) {
+  return drivers.filter(function (driver) {return driver[Object.keys(obj)[0]] === obj[Object.keys(obj)[0]];});
+};
+
+function exactMatchToList(drivers, obj) {
+  let filteredDrivers = exactMatch(drivers, obj);
+  return filteredDrivers.map(function (driver) {return driver.name;});
+};
