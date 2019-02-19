04c3c66c3dbdf32d6bde0771253780d79f6b9b6f
Dusty211 <kylebenz@hotmail.com>
2019-02-19 08:10:44 -0500

    formatting

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 // Code your solution here:
+
 function driversWithRevenueOver(drivers, revenue) {
-  return drivers.filter(function(driver) { return driver.revenue > revenue; });
+  return drivers.filter(function(driver) {
+    return driver.revenue > revenue;
+  });
 }
 
 function driverNamesWithRevenueOver(drivers, revenue) {
-  const driverObjects = drivers.filter(function(driver) { return driver.revenue > revenue; });
-  return driverObjects.map(function (driver) { return driver.name; });
+  const driverObjects = drivers.filter(function(driver) {
+    return driver.revenue > revenue;
+  });
+  return driverObjects.map(function (driver) {
+    return driver.name;
+  });
 }
 
 function exactMatch(drivers, object) {
-  return drivers.filter(function(driver) { return driver[Object.keys(object)[0]] === Object.values(object)[0]; });
+  return drivers.filter(function(driver) {
+    return driver[Object.keys(object)[0]] === Object.values(object)[0];
+  });
 }
 
 function exactMatchToList(drivers, object) {
-  const driverObjects = drivers.filter(function(driver) { return driver[Object.keys(object)[0]] === Object.values(object)[0]; });
-  return driverObjects.map(function (driver) { return driver.name; });
+  const driverObjects = drivers.filter(function(driver) {
+    return driver[Object.keys(object)[0]] === Object.values(object)[0];
+  });
+  return driverObjects.map(function (driver) {
+    return driver.name;
+  });
 }
